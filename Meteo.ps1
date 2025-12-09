6da71660ad49541a73bc3a9b8e85cb437f2287b3
L11177 <Luka.PASAOGLU@student.hel.be>
2025-12-09 19:56:50 +0100

    Version humaine

---
+++ Meteo.ps1
@@ -0,0 +1,14 @@
+﻿$ville = Read-Host "Entrez une ville"
+
+# Récupérer latitude & longitude de la ville
+$geo = Invoke-RestMethod "https://geocoding-api.open-meteo.com/v1/search?name=$ville&count=1"
+
+# Récupérer la météo actuelle avec latitude & longitude
+$lat = $geo.results[0].latitude
+$lon = $geo.results[0].longitude
+
+$meteo = Invoke-RestMethod "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&current_weather=true&temperature_unit=celsius"
+
+# Affichage simple
+$temp = $meteo.current_weather.temperature
+Write-Host "La température à $ville est de $temp °C."
